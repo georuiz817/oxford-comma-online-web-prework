f9a14c3bab96f2e84b6976062361b4ae2f659bcf
George Ruiz <georuiz817@gmail.com>
2019-05-28 20:48:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+# and converts it into a string using the Oxford comma.
 def oxford_comma(array)
-
+   if array.length == 1
+      array.join
+   elsif array.length == 2
+      array.join(" and ")
+   else
+      lastStr = array.pop
+      array.join(", ") << ", and #{lastStr}"
+      
+   end
 end
